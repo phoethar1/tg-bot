42df351d7563238b888081639a00c785438fc92d
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-12-26 22:37:25 +0630

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://www.pornhub.com/view_video.php?viewkey=ph5e39db12a3461"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://www.pornhub.com"),
+                    InlineKeyboardButton("Support Group", url="https://www.pornhub.com")
                 ]
             ]
         ),
